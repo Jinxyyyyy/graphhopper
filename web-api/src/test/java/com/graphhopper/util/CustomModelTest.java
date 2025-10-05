d962acf3a6b669c9c417072d2c18411e14a54f30
Yamir A. Poldo Silva <yamira.poldosilva@MacBookPro-de-Yamir.local>
2025-10-05 13:37:54 -0400

    commit 3 test + config pitest

--- web-api/src/test/java/com/graphhopper/util/CustomModelTest.java
+++ web-api/src/test/java/com/graphhopper/util/CustomModelTest.java
@@ -19,13 +19,17 @@
 package com.graphhopper.util;
 
 import com.graphhopper.json.Statement;
+
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
 
 import java.util.Iterator;
 
+
 import static com.graphhopper.json.Statement.*;
 import static com.graphhopper.json.Statement.Op.MULTIPLY;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomModelTest {
 
@@ -70,5 +74,38 @@ public class CustomModelTest {
         iter = CustomModel.merge(car, emptyCar).getPriority().iterator();
         assertEquals("0.5", iter.next().value());
         assertEquals("0.8", iter.next().value());
+    }
+    /**
+     * Test testSetAndGetDistanceInfluence
+     *
+     * Intention :
+     * Vérifier que la valeur de distanceInfluence est correctement enregistrée
+     * et récupérée.
+     *
+     * Motivation des données de test :
+     * - null : confirme qu’aucune valeur n’est définie initialement.
+     * - 2.5, 4.0, 6.5 : valeurs arbitraires mais distinctes, utilisées pour tester
+     *   l’enregistrement, la modification et la lecture cohérente des valeurs.
+     *
+     * Oracle (résultat attendu) :
+     * - getDistanceInfluence() retourne null par défaut.
+     * - Après appel à setDistanceInfluence(...), getDistanceInfluence() retourne la valeur donnée.
+     * - Le setter retourne l’instance courante.
+     */
+    @Test
+    public void testSetAndGetDistanceInfluence() {
+        CustomModel car = new CustomModel();
+        assertNull(car.getDistanceInfluence());
+
+        car.setDistanceInfluence(2.5);
+        assertEquals(2.5, car.getDistanceInfluence(), 0.0001);
+
+        car.setDistanceInfluence(4.0);
+        assertEquals(4.0, car.getDistanceInfluence(), 0.0001);
+
+        CustomModel car2 = car.setDistanceInfluence(6.5);
+        assertSame(car, car2);
+        assertEquals(6.5, car.getDistanceInfluence(), 0.0001);
+
     }
 }
